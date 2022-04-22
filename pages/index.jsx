a97ae24885e9373e3200b2b939fcd8897e4f314e
perpetualgrimace <hello@jamesferrell.me>
2022-04-22 17:50:32 -0400

    extracts nav to component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@
 // import Button from '../components/Button';
 // import ClickCount from '../components/ClickCount';
 import GlobalHead from '/components/GlobalHead';
+import GlobalNav from '/components/GlobalNav';
 
 import OutboundIcon from '../components/icons/OutboundIcon';
 
@@ -13,26 +14,7 @@ function Home() {
         description="Data science toolkit"
       />
 
-      <div className="nav">
-        <nav className="nav-inner wrapper" role="navigation">
-          <a className="nav-logo" href="">
-            <img src="/logo-horizontal.png" srcSet="/logo-horizontal.png 1x, /logo-horizontal@2x.png 2x" alt="Mudavar, home" draggable="false" />
-          </a>
-
-          <ul className="nav-list">
-            {/* <li className="nav-item">
-              <a className="nav-link u-font-xs" href="">
-                Asset viewer
-              </a>
-            </li> */}
-            <li className="nav-item">
-              <a className="nav-link u-font-xs" aria-current="page" href="">
-                Tools
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <GlobalNav />
 
       <main className="main" id="#main">
         <header className="hero" role="banner">
