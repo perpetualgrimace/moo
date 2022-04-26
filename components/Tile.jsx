3331cd54a19ed4bbdc69899663ffc2af9ceeec34
perpetualgrimace <hello@jamesferrell.me>
2022-04-26 18:58:30 -0400

    adds layout & more utilities

--- components/Tile.jsx
+++ components/Tile.jsx
@@ -2,17 +2,17 @@ import OutboundIcon from '/components/icons/OutboundIcon';
 
 export default function Tile(props) {
   const {href, outbound, imgSrc, label, altText} = props;
-  
+
   return(
-    <a className="tile darkglass" href={href}>
+    <a className="tile darkglass u-mb-md" href={href}>
       {outbound &&
         <div className="tile-icon">
           <OutboundIcon />
         </div>
       }
-      <div className="tile-inner">
-        <img 
-          className="tile-img" 
+      <div className="tile-inner u-m-auto">
+        <img
+          className="tile-img u-mh-auto"
           src={`/icons/tools/${imgSrc}.png`}
           srcSet={`/icons/tools/${imgSrc}.svg`}
           alt={altText}
--- components/ToolsSection.jsx
+++ components/ToolsSection.jsx
@@ -1,7 +1,7 @@
 export default function ToolsSection(props) {
   const {sectionTitle, children} = props;
   return (
-    <section className="tools-section">
+    <section className="tools-section u-mb-lg">
       <h2 className="tools-heading">{ sectionTitle }</h2>
       <ul className="tools-list">
         {children}
