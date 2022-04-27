1c1604c8f62cb068d70d3c99e8c7c271fece246a
perpetualgrimace <hello@jamesferrell.me>
2022-04-27 16:41:00 -0400

    moves bg style to separate stylesheet

--- components/SpaceBG.jsx
+++ components/SpaceBG.jsx
@@ -1,7 +1,7 @@
 export default function SpaceBG() {
   return(
-    <div className="bg">
-      <img className="bg-img" src="/bg.jpg" srcSet="/bg.jpg 1x, /bg@2x.jpg 2x" alt="" draggable="false" />
+    <div className="space-bg">
+      <img className="space-bg-img" src="/bg.jpg" srcSet="/bg.jpg 1x, /bg@2x.jpg 2x" alt="" draggable="false" />
     </div>
   )
 }
