c33f456bd37826bcac5e943519cc32b01f928a0e
perpetualgrimace <hello@jamesferrell.me>
2022-04-29 17:26:47 -0400

    abstracts layout to DefaultLayout component

---
+++ components/DefaultLayout.jsx
@@ -0,0 +1,33 @@
+import Meta from '/components/Meta';
+import Navbar from '/components/Navbar';
+import Hero from '/components/Hero';
+
+import SpaceBG from '../components/SpaceBG';
+
+export default function DefaultLayout(props) {
+  const {title, slug, children} = props;
+
+  return (
+    <div className={`${slug}-layout default-layout`}>
+      <Meta
+        pageTitle={title}
+        description="Data science toolkit"
+      />
+
+      <Navbar />
+
+      <main className="main" id="#main">
+        <Hero
+          pageTitle={title}
+          imgSlug={slug}
+        />
+
+        <div className={`${slug}-wrapper wrapper`}>
+          {children}
+        </div>
+      </main>
+
+      <SpaceBG />
+    </div>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,81 +1,57 @@
-import Meta from '/components/Meta';
-import Navbar from '/components/Navbar';
-import Hero from '/components/Hero';
-
+import DefaultLayout from '/components/DefaultLayout';
 import ToolsSection from '/components/ToolsSection';
 import ToolsItem from '/components/ToolsItem';
 
-import SpaceBG from '../components/SpaceBG';
-
-function Home() {
-  const title="Data Science Toolkit";
-
+export default function Home() {
   return (
-    <div className="home">
-      <Meta
-        pageTitle={title}
-        description="Data science toolkit"
-      />
-
-      <Navbar />
-
-      <main className="main" id="#main">
-        <Hero
-          pageTitle={title}
-          imgSlug="tools"
+    <DefaultLayout
+      title="Data Science Toolkit"
+      slug="tools"
+    >
+      <ToolsSection sectionTitle="Data science">
+        <ToolsItem
+          href="#"
+          outbound={true}
+          imgSrc="jupyterhub-logo"
+          altText="Jupyter Hub"
         />
+      </ToolsSection>
 
-        <div className="tools-wrapper wrapper">
-          <ToolsSection sectionTitle="Data science">
-            <ToolsItem
-              href="#"
-              outbound={true}
-              imgSrc="jupyterhub-logo"
-              altText="Jupyter Hub"
-            />
-          </ToolsSection>
+      <ToolsSection sectionTitle="Data analysis">
+        <ToolsItem
+          href="#"
+          outbound={true}
+          imgSrc="business-intelligence-icon"
+          label="Business intelligence view"
+          altText=""
+        />
+        <ToolsItem
+          href="#"
+          outbound={true}
+          imgSrc="graph-view-icon"
+          label="Graph view"
+          altText=""
+        />
+      </ToolsSection>
 
-          <ToolsSection sectionTitle="Data analysis">
-            <ToolsItem
-              href="#"
-              outbound={true}
-              imgSrc="business-intelligence-icon"
-              label="Business intelligence view"
-              altText=""
-            />
-            <ToolsItem
-              href="#"
-              outbound={true}
-              imgSrc="graph-view-icon"
-              label="Graph view"
-              altText=""
-            />
-          </ToolsSection>
-
-          <ToolsSection sectionTitle={<>
-            Browse & manage <span className="u-visually-hidden">data</span>
-          </>}>
-            <ToolsItem
-              href="#"
-              outbound={true}
-              imgSrc="data-viewer-icon"
-              label="Data viewer"
-              altText=""
-            />
-            <ToolsItem
-              href="#"
-              outbound={true}
-              imgSrc="prediction-engines-icon"
-              label="Engines"
-              altText=""
-            />
-          </ToolsSection>
-        </div>
-      </main>
-
-      <SpaceBG />
-    </div>
+      <ToolsSection sectionTitle={<>
+        Browse & manage <span className="u-visually-hidden">data</span>
+      </>}>
+        <ToolsItem
+          href="#"
+          outbound={true}
+          imgSrc="data-viewer-icon"
+          label="Data viewer"
+          altText=""
+        />
+        <ToolsItem
+          href="#"
+          outbound={true}
+          imgSrc="prediction-engines-icon"
+          label="Engines"
+          altText=""
+        />
+      </ToolsSection>
+    </DefaultLayout>
   )
 }
-
-export default Home;
