3a83c78038b1e599ae76aca4eee8d827968733f3
perpetualgrimace <hello@jamesferrell.me>
2022-05-04 15:12:31 -0400

    adds breadcrumb to hero

--- components/DefaultLayout.jsx
+++ components/DefaultLayout.jsx
@@ -1,11 +1,19 @@
+import { useRouter } from "next/router";
+
 import Meta from "/components/Meta";
 import Navbar from "/components/Navbar";
 import Hero from "/components/Hero";
-
 import SpaceBG from "../components/SpaceBG";
 
 export default function DefaultLayout(props) {
   const { title, slug, children } = props;
+
+  const currRoute = useRouter().pathname;
+  let parentSlug, parentTitle;
+  if (currRoute.split("/").length === 3) {
+    parentSlug = `/${currRoute.split("/")[1]}`;
+    parentTitle = parentSlug === "/assets" ? "Asset Viewer" : parentSlug;
+  }
 
   return (
     <div className={`${slug}-layout default-layout`}>
@@ -14,7 +22,12 @@ export default function DefaultLayout(props) {
       <Navbar />
 
       <main className="main" id="#main">
-        <Hero pageTitle={title} imgSlug={slug} />
+        <Hero
+          pageTitle={title}
+          imgSlug={slug}
+          parentSlug={parentSlug}
+          parentTitle={parentTitle}
+        />
 
         <div className={`${slug}-wrapper wrapper`}>{children}</div>
       </main>
