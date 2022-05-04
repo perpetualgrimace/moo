39cb7d6177c2c93577bc5cbedd0b111e531cdabc
perpetualgrimace <hello@jamesferrell.me>
2022-05-04 14:18:00 -0400

    redirects home to assets list page

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/assets",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
-}
+};
