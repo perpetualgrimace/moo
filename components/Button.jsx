37c8676d68873624460523681643ad948a6e4819
perpetualgrimace <hello@jamesferrell.me>
2022-05-10 11:01:32 -0400

    allows Button component to be used as a link

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Button(props) {
-  return <button type="button" className="btn" {...props} />;
+  const { href, type, fontSize, children } = props;
+
+  const Component = href ? (
+    <Link href={href} {...props} passHref>
+      <a className={`button u-font-${fontSize || "md"}`}>{children}</a>
+    </Link>
+  ) : (
+    <button
+      className={`button u-font-${fontSize || "md"}`}
+      {...props}
+      type={type || "button"}
+    >
+      {children}
+    </button>
+  );
+
+  return Component;
 }
