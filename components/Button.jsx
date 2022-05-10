97d2d4561572ff2725cad0d3b923ee870b1b5f33
perpetualgrimace <hello@jamesferrell.me>
2022-05-10 12:18:33 -0400

    styles button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,18 +1,40 @@
 import Link from "next/link";
 
+const defaults = {
+  fontSize: "sm",
+  theme: "default",
+};
+
+function generateClassList(props) {
+  const { className, fontSize, theme, fill } = props;
+
+  let classList = "button";
+
+  if (className) classList += ` ${className}`;
+
+  classList += ` u-font-${fontSize || defaults.fontSize}`;
+  classList += ` ${theme || defaults.theme}-theme`;
+
+  if (fill) classList += ` fill`;
+
+  return classList;
+}
+
 export default function Button(props) {
-  const { href, type, fontSize, children } = props;
+  const { href, type, children } = props;
+
+  const filteredProps = {
+    ...props,
+    fill: undefined,
+    className: generateClassList(props),
+  };
 
   const Component = href ? (
-    <Link href={href} {...props} passHref>
-      <a className={`button u-font-${fontSize || "md"}`}>{children}</a>
+    <Link href={href} passHref>
+      <a {...filteredProps}>{children}</a>
     </Link>
   ) : (
-    <button
-      className={`button u-font-${fontSize || "md"}`}
-      {...props}
-      type={type || "button"}
-    >
+    <button {...filteredProps} type={type || "button"}>
       {children}
     </button>
   );
