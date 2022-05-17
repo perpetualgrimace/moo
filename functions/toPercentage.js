9678a8375d3a1573ad3f093e5df52f46d50d9138
perpetualgrimace <hello@jamesferrell.me>
2022-05-17 15:35:48 -0400

    simplifies percentage helper

--- functions/toPercentage.js
+++ functions/toPercentage.js
@@ -3,11 +3,7 @@ export default function toPercentage(str) {
   if (str) {
     const decimal = (str * 100).toFixed(1);
 
-    if (decimal - Math.floor(decimal) !== 0) {
-      return `${decimal}%`;
-    } else {
-      return `${decimal.split(".")[0]}%`;
-    }
+    return `${decimal.split(".")[0]}%`;
   }
   return "missing `str` in `toDecimal()`";
 }
