a4fa533bfe2c9edb492763b492bebe3c7bdf99ad
perpetualgrimace <hello@jamesferrell.me>
2022-05-17 14:08:17 -0400

    styles DataLakeTable

--- components/DataLakeTable.jsx
+++ components/DataLakeTable.jsx
@@ -4,26 +4,26 @@ export default function DataLakeTable(props) {
   const { data } = props;
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Created</th>
-          <th>Size</th>
-          <th>Data mart</th>
+    <table className="data-lake-table">
+      <thead className="data-lake-thead">
+        <tr className="data-lake-tr">
+          <th className="data-lake-th">Name</th>
+          <th className="data-lake-th">Created</th>
+          <th className="data-lake-th">Size</th>
+          <th className="data-lake-th">Data mart</th>
         </tr>
       </thead>
       <tbody>
         {data.map((table) => (
-          <tr key={table.id}>
-            <td>
+          <tr className="data-lake-tr" key={table.id}>
+            <td className="data-lake-td">
               <Link href={`/assets/data/table/${table.id}`}>
-                <a>{table.name}</a>
+                <a className="data-lake-link">{table.name}</a>
               </Link>
             </td>
-            <td>{table.created}</td>
-            <td>{table.size}</td>
-            <td>{table.mart}</td>
+            <td className="data-lake-td">{table.created}</td>
+            <td className="data-lake-td">{table.size}</td>
+            <td className="data-lake-td">{table.mart}</td>
           </tr>
         ))}
       </tbody>
