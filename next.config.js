b16c86d32781e1933e638f4c05e911d8878e5679
David Montaño <david@andalusia.ai>
2022-07-01 23:27:59 -0500

    - some fixes to work with http://averroes.andal.us:8000/ which is current prod

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
     apiUrl:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000/api" // development api
-        : "http://localhost:3000/api", // production api
+        : "http://averroes.andal.us:8000/api", // production api
   },
   reactStrictMode: true,
 };
