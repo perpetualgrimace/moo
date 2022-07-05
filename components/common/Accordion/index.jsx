c7d5096f6899383943a33199df37b3a5b35fbec4
perpetualgrimace <hello@jamesferrell.me>
2022-07-05 16:37:52 -0400

    handles empty accordions

--- components/common/Accordion/index.jsx
+++ components/common/Accordion/index.jsx
@@ -1,4 +1,12 @@
+import WarningIcon from "/components/icons/WarningIcon";
+
 export default function Accordion(props) {
   const { children } = props;
-  return <div className="accordion">{children}</div>;
+  return children.length ? (
+    <div className="accordion">{children}</div>
+  ) : (
+    <p className="u-font-lg">
+      <WarningIcon /> No items match this selection
+    </p>
+  );
 }
